1833476f447a5ebb48d84d937fdced9189c45815
Juan Carlos <jcrpelaez@yahoo.com>
2020-01-14 14:16:12 -0600

    added parse for new friends

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,13 +1,32 @@
 console.log("API Route Connected Successfully");
 
+// Links friends data
 var friends = require("../data/friends.js");
 
+// Sets function with both routes
 function apiRoutes(app) {
+  
+  //created get route  
   app.get("/api/friends", function(req, res) {
     res.json(friends);
   });
+
+  //created post route to handle survey results
   app.post("/api/friends", function (req, res) {
     
+    var newFriend ={
+      name: req.body.name,
+      photo: req.body.phot,
+      scores:[]
+    };
+    console.log(scores)
+
+    var scoresArray = [];
+    for (var i = 0; i <req.body.scores.length;i++){
+      scoresArray.push(parseInt(req.body.scores[i]))
+    }
+    newFriend.scores = scoresArray
+
   });
   
 }
